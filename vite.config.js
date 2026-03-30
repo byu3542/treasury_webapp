f518d0bd7a5bacf4704f6299dd31b830f441e9f1
byu3542 <45214928+byu3542@users.noreply.github.com>
2026-03-30 15:27:15 -0600

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/treasury_webapp/',
   build: {
     outDir: 'dist',
     sourcemap: false,
